d0791cb9f9e92fb884c20008c9d83db6caddc4ee
Pedro Henrique Monteiro <pedro.silva10@iesb.edu.br>
2024-03-01 21:55:41 -0300

    backend

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/hello', function(req, res){
+    res.send('Chegou a resposta')
+})
+
+
+
+app.listen(3000, function(){
+    console.log('Server up post 3000')
+})
